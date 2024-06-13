2c7f9095bcd1d302ddfee57daed8c7011a9a5f83
Monosix8 <162055149+Monosix8@users.noreply.github.com>
2024-06-13 20:07:40 +0700

    using namespace

--- BinarySearchTree_0028/BinarySearchTree_0028.cpp
+++ BinarySearchTree_0028/BinarySearchTree_0028.cpp
@@ -2,17 +2,3 @@
 #include <string>
 using namespace std;
 
-class Node
-{
-	string info;
-	Node* leftchild;
-	Node* rightchild;
-
-	// Constructor fir the node class
-	Node(string i, Node* l, Node* r)
-	{
-		info = i;
-		leftchild = l;
-		rightchild = r;
-	}
-};
