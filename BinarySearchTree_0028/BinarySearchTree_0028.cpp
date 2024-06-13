8bb6975647e70c838dd02c4495b422c06275f684
Monosix8 <162055149+Monosix8@users.noreply.github.com>
2024-06-13 20:21:48 +0700

    if

--- BinarySearchTree_0028/BinarySearchTree_0028.cpp
+++ BinarySearchTree_0028/BinarySearchTree_0028.cpp
@@ -39,4 +39,21 @@ public:
 		Node* currentNode = nullptr;
 		search(element, parent, currentNode); // Locate the node which to the parrent of the node to be inserted
 
-		
+		if (parent == nullptr) // If the parent is NULL (Tree is empty)
+		{
+			ROOT = newNode;; // Mark the new node as ROOT
+			return; //Exit
+		}
+
+		if (element < parent->info) //If the value in the data field of the new node is less than of the parent
+		{
+			parent->leftchild = newNode; //Make the left child of the parent point to the new node
+		}
+		else if (element > parent->info) // If the value in the data field of the new data is greater than that of the parent
+		{
+			parent->rightchild = newNode; //Make the right child of the parent point to the new node
+		}
+	}
+
+
+	
