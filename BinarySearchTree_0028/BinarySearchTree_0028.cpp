7a98c31bb45cfc4419791f7fcef791c338d2163e
Monosix8 <162055149+Monosix8@users.noreply.github.com>
2024-06-13 20:24:52 +0700

    char ch

--- BinarySearchTree_0028/BinarySearchTree_0028.cpp
+++ BinarySearchTree_0028/BinarySearchTree_0028.cpp
@@ -123,4 +123,16 @@ int main()
 	BinaryTree obj;
 	while (true)
 	{
+		cout << "\nMneu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inordeer traversal" << endl;
+		cout << "3. Perform preorder traversal" << endl;
+		cout << "4. Perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+
 		
